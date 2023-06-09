a2dae30d348dd01d4061caee57722dcab52f74e1
kevinzhang <2633915958@qq.com>
2023-06-09 17:12:26 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -108,36 +108,30 @@ impl Api {
                 }))
             })?;
             if !IMAGE_TYPE.contains(&file_name.split(".").last().unwrap()) {
-                return Err(
-                    UploadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: "上传的文件不是图片".to_string(),
-                        data: json!(null),
-                    }))
-                    .into(),
-                );
+                return Err(UploadResponse::InternalServerError(Json(ResultVo {
+                    code: 500,
+                    msg: "上传的文件不是图片".to_string(),
+                    data: json!(null),
+                }))
+                .into());
             }
             if data.len() > 200 * 1024 {
-                return Err(
-                    UploadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: "文件大小超过200kb".to_string(),
-                        data: json!(null),
-                    }))
-                    .into(),
-                );
+                return Err(UploadResponse::InternalServerError(Json(ResultVo {
+                    code: 500,
+                    msg: "文件大小超过200kb".to_string(),
+                    data: json!(null),
+                }))
+                .into());
             }
             files.push((file_name, data));
             multipart_count += 1;
             if multipart_count > 4 {
-                return Err(
-                    UploadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: "上传的文件数量超过4个".to_string(),
-                        data: json!(null),
-                    }))
-                    .into(),
-                );
+                return Err(UploadResponse::InternalServerError(Json(ResultVo {
+                    code: 500,
+                    msg: "上传的文件数量超过4个".to_string(),
+                    data: json!(null),
+                }))
+                .into());
             }
         }
         let mut file_read_dir = tokio::fs::read_dir(&dir_path).await.map_err(|e| {
@@ -159,23 +153,23 @@ impl Api {
         }
         let file_count = file_name_vec.len() as u8;
         if file_count + multipart_count > 4 {
-            return Err(
-                UploadResponse::InternalServerError(Json(ResultVo {
-                    code: 500,
-                    msg: "上传的文件数量超过4个".to_string(),
-                    data: json!(null),
-                }))
-                .into(),
-            );
+            return Err(UploadResponse::InternalServerError(Json(ResultVo {
+                code: 500,
+                msg: "上传的文件数量超过4个".to_string(),
+                data: json!(null),
+            }))
+            .into());
         }
         for (file_name, data) in files {
-            tokio::fs::write(format!("{}/{}", dir_path, file_name), data).await.map_err(|e| {
-                UploadResponse::InternalServerError(Json(ResultVo {
-                    code: 500,
-                    msg: format!("写入文件失败: {}", e),
-                    data: json!(null),
-                }))
-            })?;
+            tokio::fs::write(format!("{}/{}", dir_path, file_name), data)
+                .await
+                .map_err(|e| {
+                    UploadResponse::InternalServerError(Json(ResultVo {
+                        code: 500,
+                        msg: format!("写入文件失败: {}", e),
+                        data: json!(null),
+                    }))
+                })?;
             file_name_vec.push(file_name);
         }
         Ok(UploadResponse::Success(Json(ResultVo {
@@ -195,40 +189,26 @@ impl Api {
         let mut paths = match tokio::fs::read_dir(&dir_path).await {
             Ok(paths) => paths,
             Err(_) => {
-                tokio::fs::create_dir_all(&dir_path).await.map_err(|e| {
-                    DownloadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: format!("在创建目录时出错: {}", e),
-                        data: json!(null),
-                    }))
-                })?;
-                tokio::fs::read_dir(&dir_path).await.map_err(|e| {
-                    DownloadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: format!("无法打开指定的目录: {}", e),
-                        data: json!(null),
-                    }))
-                })?
+                tokio::fs::create_dir_all(&dir_path)
+                    .await
+                    .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?;
+                tokio::fs::read_dir(&dir_path)
+                    .await
+                    .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?
             }
         };
         let mut paths_vec = Vec::new();
-        while let Some(path) = paths.next_entry().await.map_err(|e| {
-            DownloadResponse::InternalServerError(Json(ResultVo {
-                code: 500,
-                msg: format!("无法读取目录: {}", e),
-                data: json!(null),
-            }))
-        })? {
+        while let Some(path) = paths
+            .next_entry()
+            .await
+            .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?
+        {
             let path_str = path.file_name().into_string().unwrap();
             paths_vec.push(format!("{}/{}", &dir_path, &path_str));
         }
-        let zip_file = zip_files(&paths_vec).await.map_err(|e| {
-            DownloadResponse::InternalServerError(Json(ResultVo {
-                code: 500,
-                msg: format!("压缩文件时出错: {}", e),
-                data: json!(null),
-            }))
-        })?;
+        let zip_file = zip_files(&paths_vec)
+            .await
+            .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?;
         Ok(DownloadResponse::Success(
             Attachment::new(zip_file)
                 .filename("archive.zip")
@@ -245,13 +225,9 @@ impl Api {
     ) -> Result<DownloadResponse> {
         let dir_path = format!("/root/image/{}/{}", dir_type.deref(), dir_id.deref());
         let file_path = format!("{}/{}", dir_path, file_name.to_string());
-        let file = tokio::fs::read(&file_path).await.map_err(|e| {
-            DownloadResponse::InternalServerError(Json(ResultVo {
-                code: 500,
-                msg: format!("读取文件失败: {}", e),
-                data: json!(null),
-            }))
-        })?;
+        let file = tokio::fs::read(&file_path)
+            .await
+            .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?;
         Ok(DownloadResponse::Success(
             Attachment::new(file)
                 .filename(file_name.to_string())
@@ -265,29 +241,18 @@ impl Api {
         let mut paths = match tokio::fs::read_dir(&dir_path).await {
             Ok(paths) => paths,
             Err(_) => {
-                tokio::fs::create_dir_all(&dir_path).await.map_err(|e| {
-                    DownloadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: format!("在创建目录时出错: {}", e),
-                        data: json!(null),
-                    }))
-                })?;
-                tokio::fs::read_dir(&dir_path).await.map_err(|e| {
-                    DownloadResponse::InternalServerError(Json(ResultVo {
-                        code: 500,
-                        msg: format!("无法打开指定的目录: {}", e),
-                        data: json!(null),
-                    }))
-                })?
+                tokio::fs::create_dir_all(&dir_path)
+                    .await
+                    .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?;
+                tokio::fs::read_dir(&dir_path)
+                    .await
+                    .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?
             }
         };
-        let preview_path = paths.next_entry().await.map_err(|e| {
-            DownloadResponse::InternalServerError(Json(ResultVo {
-                code: 500,
-                msg: format!("无法读取目录: {}", e),
-                data: json!(null),
-            }))
-        })?;
+        let preview_path = paths
+            .next_entry()
+            .await
+            .map_err(|_e| DownloadResponse::Success(Attachment::new(Vec::new())))?;
         match preview_path {
             Some(preview_path) => {
                 let preview_path_str = preview_path.file_name().into_string().unwrap();
@@ -306,14 +271,7 @@ impl Api {
                         .attachment_type(AttachmentType::Attachment),
                 ))
             }
-            None => Err(
-                DownloadResponse::InternalServerError(Json(ResultVo {
-                    code: 500,
-                    msg: "目录为空".to_string(),
-                    data: json!(null),
-                }))
-                .into(),
-            ),
+            None => Ok(DownloadResponse::Success(Attachment::new(Vec::new()))),
         }
     }
 
